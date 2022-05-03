9a185574ceba81786ec25302c371c6122ae14e86
fericelli <fericelli.garcia@gmail.com>
2022-05-03 00:56:22 -0400

    solicitud

--- php/intercambios/solicitudes.php
+++ php/intercambios/solicitudes.php
@@ -4,7 +4,7 @@
 		function __construct(){
 			include("../conexion.php");
 			$this->Conexion = new Conexion("u956446715_intercash");
-			echo "{".$this->retorno()."}";
+			echo $this->retorno();
 			$this->Conexion->CerrarConexion();
 		}
 		private function retorno(){
