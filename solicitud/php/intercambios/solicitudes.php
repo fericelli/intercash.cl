7635796468cec69488a3e1e0c0d7ecd30f8f6c59
fericelli <fericelli.garcia@gmail.com>
2023-06-05 16:30:20 -0400

    correccion

--- solicitud/php/intercambios/solicitudes.php
+++ solicitud/php/intercambios/solicitudes.php
@@ -2,7 +2,8 @@
 	Class Solicitudes{
 		private $Conexion;
 		function __construct(){
-			include("../conexion.php");
+			
+			include("../../../php/conexion.php");
 			$this->Conexion = new Conexion();
 			echo "[".$this->retorno()."]";
 			$this->Conexion->CerrarConexion();
