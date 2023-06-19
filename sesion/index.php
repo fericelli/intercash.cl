e568f6e237ad5ef770c2e4ba41d1a00d9fd750a5
fericelli <fericelli.garcia@gmail.com>
2023-06-19 10:45:28 -0400

    logo

--- sesion/index.php
+++ sesion/index.php
@@ -11,6 +11,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="shortcut icon" href="../imagenes/logo.ico"> 
         <link href="https://file.myfontastic.com/iKa94pqcdMLv4DmY2UYJVK/icons.css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" >
