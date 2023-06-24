0bcc45c632bc75bc65c96b7e6266fe9167dec75c
fericelli <fericelli.garcia@gmail.com>
2023-06-24 19:25:48 -0400

    a

--- ipingresos/index.php
+++ ipingresos/index.php
@@ -7,7 +7,7 @@
             echo $_SERVER['HTTP_X_FORWARDED_FOR']."<br>";
             echo $_SERVER['REMOTE_ADDR']."<br>";
             echo $_SERVER['HTTP_CLIENT_IP']."<br>";
-            echo $_SERVER['HTTP_X_FORWARDED_FOR']."<br>";
+            echo $_SERVER['HTTP_X_FORWARDED_FOR']."<br>";  
         }
   }
   new Index();
