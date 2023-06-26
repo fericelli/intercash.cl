a2f54d161fe8b0f49b055420ce2930941b727e8d
fericelli <fericelli.garcia@gmail.com>
2023-06-26 18:42:29 -0400

    a

--- php/configuracion/agregarbancos.php
+++ php/configuracion/agregarbancos.php
@@ -11,19 +11,24 @@
              $json = json_decode($_POST["datos"],TRUE);
              $cantidad = 1;
              foreach($json as $datos){
-                $codigo="";
+                /*$codigo="";
                 if(strlen($cantidad)==1){
                     $codigo = "00".strval($cantidad);
                 }else if(strlen($cantidad>1)){
                     $codigo = "0".strval($cantidad);
+                }*/
+                if(strpos($datos, "(aún no redactado)")){
+                    $nombre = trim(substr($datos,0,strpos($datos, "(aún no redactado)")));
+                    $consultar = $this->Conexion->Consultar("SELECT * FROM bancos WHERE nombre LIKE '".$nombre."%' AND pais='".$_POST["pais"]."'");
+                    if($this->Conexion->Recorrido($consultar)){
+                        $sql = "UPDATE bancos SET nombre='".$nombre."' WHERE nombre LIKE '".$nombre."%' AND pais='".$_POST["pais"]."'";
+                        //echo $sql = "INSERT INTO bancos (codigo,nombre,pais) VALUES ('".$codigo."','".trim($datos)."','".$_POST["pais"]."')\n";
+                        echo $this->Conexion->Consultar($sql);
+                    }
                 }
-                $consultar = $this->Conexion->Consultar("SELECT * FROM bancos WHERE nombre='".trim($datos)."' AND pais='".$_POST["pais"]."'");
-                if(!$this->Conexion->Recorrido($consultar)){
-                        //echo "INSERT INTO bancos (codigo,nombre,pais) VALUES ('".$codigo."','".trim($datos)."','".$_POST["pais"]."')\n";
-                    echo $this->Conexion->Consultar("INSERT INTO bancos (codigo,nombre,pais) VALUES ('".$codigo."','".trim($datos)."','".$_POST["pais"]."')");
-                }
+                /*
                
-                $cantidad ++;
+                $cantidad ++;*/
                 
              }
         }
