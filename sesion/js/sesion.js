6f4a7134a78f39b265ac6b5ce533ec56cf8a16fa
fericelli <fericelli.garcia@gmail.com>
2023-06-27 13:22:48 -0400

    c

--- sesion/js/sesion.js
+++ sesion/js/sesion.js
@@ -25,7 +25,7 @@ $(document).on("ready",function(){
    
     
         $(".item").on("click",function(){ 
-            if($(".contenido-imagen").css("display")=="none"){
+            if($(".contenido-imagen").css("display")=="none" && $("#main-container").html()!=""){
                 $(".contenido-screeshot").remove();
                 $(".script").remove();
                 
@@ -622,6 +622,7 @@ $(document).on("ready",function(){
                     });
                 } 
             }
+        
         })
     
 
