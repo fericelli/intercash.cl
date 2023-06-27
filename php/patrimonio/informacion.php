e05eb354caeab9e92d8ab674e9dedbc218a1e190
fericelli <fericelli.garcia@gmail.com>
2023-06-27 13:20:09 -0400

    s

--- php/patrimonio/informacion.php
+++ php/patrimonio/informacion.php
@@ -8,7 +8,27 @@
 			$this->Conexion->CerrarConexion();
 		}
 		private function retorno(){
-			$this->Conxion->Consultar("SELECT * FROM intercangios");dd
+			$selectormonedas = [];
+			$monedas = [];
+			$consultar = $this->Conexion->Consultar("SELECT *  FROM monedas");
+			while($moneda = $this->Conexion->Recorrido($consultar)){
+				array_push($selectormonedas,'"'.$moneda["iso_moneda"].'"');
+				$monedas[$moneda["iso_moneda"]]["moneda"] = $moneda["iso_moneda"];
+				$consultar1 = $this->Conexion->Consultar("SELECT SUM(monto) FROM operaciones WHERE moneda='".$moneda["iso_moneda"]."' AND operacion='venta'");
+				$consultar2 = $this->Conexion->Consultar("SELECT SUM(monto) FROM operaciones WHERE moneda='".$moneda["iso_moneda"]."' AND operacion='compra'");
+				if($cantidad1 = $this->Conexion->Recorrido($consultar1)){
+					$monedas[$moneda["iso_moneda"]]["decimales"] = $moneda["decimales"];
+					$monedas[$moneda["iso_moneda"]]["operaciones"]["venta"] = number_format($cantidad1[0],$moneda["decimales"],".","");
+					$monedas[$moneda["iso_moneda"]]["operaciones"]["compra"] = number_format($cantidad2[0],$moneda["decimales"],".","");;
+				}
+			}
+			$fiats = [];
+			$consultar3 = $this->Conexion->Consultar("SELECT * FROM paises WHERE receptor IS NOT NULL");
+			while($fiat = $this->Conexion->Recorrido($consultar3)){
+				$fiats[$fiat["iso_moneda"]]["moneda"] = $fiat["iso2"];
+			}
+			
+			var_dump($fiats);
 		} 
 	}
 	new Informacion();
