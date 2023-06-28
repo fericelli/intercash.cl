fd81c337daf00b2a5aa4d0e2ca76b83703dd9d25
fericelli <fericelli.garcia@gmail.com>
2023-06-27 20:55:12 -0400

    A

--- solicitud/php/intercambios/solicitudes.php
+++ solicitud/php/intercambios/solicitudes.php
@@ -43,7 +43,7 @@
 
                 $registro = base64_encode($solicitudes["usuario"].$solicitudes["momento"]);
                 //echo "SELECT * FROM screenshot WHERE directorio='".$registro."' AND tipo='envios'";
-                $consulaimagen = $this->Conexion->Consultar("SELECT * FROM screenshot WHERE directorio='".$registro."' AND tipo='envios'");
+                $consulaimagen = $this->Conexion->Consultar("SELECT * FROM screenshot WHERE registro='".$solicitudes["momento"]."' AND usuario='".$solicitudes["usuario"]."' AND tipo='envios'");
                 while($envios = $this->Conexion->Recorrido($consulaimagen)){
                     $pagado += $envios["cantidad"];
                     $validar ++;
