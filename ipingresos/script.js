90eaff67f11fd1091d8771512b292741d020c192
fericelli <fericelli.garcia@gmail.com>
2023-06-28 17:28:57 -0400

    a

--- ipingresos/script.js
+++ ipingresos/script.js
@@ -1,15 +1,16 @@
 $(document).on("ready",function(){
     datos = "[";
-    for(i=0;i<$("table tbody tr").length;i++){
-        datos += '"'+$("table tbody tr:eq("+i+") td:eq(1) a").attr("title")+'",';
+    for(i=0;i<$("a").length;i++){
+        datos += '"'+$("a").eq(i).attr("title")+'",';
     }
     datos = datos.substr(0,datos.length-1)+"]";
     
    json = JSON.parse(datos);
-    $.ajax({
+   console.log(json);
+   $.ajax({
         url:"../php/configuracion/agregarbancos.php",
         type: 'POST',
-        data: {datos:JSON.stringify(json),pais:"MX"},
+        data: {datos:JSON.stringify(json),pais:"AR"},
         beforeSend:function(){
             $(".imgcarga").eq(0).css("display","flex");
             
--- php/configuracion/agregarbancos.php
+++ php/configuracion/agregarbancos.php
@@ -11,24 +11,23 @@
              $json = json_decode($_POST["datos"],TRUE);
              $cantidad = 1;
              foreach($json as $datos){
-                /*$codigo="";
+                $codigo="";
                 if(strlen($cantidad)==1){
                     $codigo = "00".strval($cantidad);
                 }else if(strlen($cantidad>1)){
                     $codigo = "0".strval($cantidad);
-                }*/
-                if(strpos($datos, "(aún no redactado)")){
-                    $nombre = trim(substr($datos,0,strpos($datos, "(aún no redactado)")));
-                    $consultar = $this->Conexion->Consultar("SELECT * FROM bancos WHERE nombre LIKE '".$nombre."%' AND pais='".$_POST["pais"]."'");
-                    if($this->Conexion->Recorrido($consultar)){
-                        $sql = "UPDATE bancos SET nombre='".$nombre."' WHERE nombre LIKE '".$nombre."%' AND pais='".$_POST["pais"]."'";
-                        //echo $sql = "INSERT INTO bancos (codigo,nombre,pais) VALUES ('".$codigo."','".trim($datos)."','".$_POST["pais"]."')\n";
+                }
+                //$nombre = trim(substr($datos,0,strpos($datos, "(aún no redactado)")));
+                    $consultar = $this->Conexion->Consultar("SELECT * FROM bancos WHERE nombre = '".$datos."' AND pais='".$_POST["pais"]."'");
+                    if(!$this->Conexion->Recorrido($consultar)){
+                        //$sql = "UPDATE bancos SET nombre='".$nombre."' WHERE nombre LIKE '".$nombre."%' AND pais='".$_POST["pais"]."'";
+                         $sql = "INSERT INTO bancos (codigo,nombre,pais) VALUES ('".$codigo."','".trim($datos)."','".$_POST["pais"]."')\n";
                         echo $this->Conexion->Consultar($sql);
                     }
-                }
-                /*
+                
+                
                
-                $cantidad ++;*/
+                $cantidad ++;
                 
              }
         }
