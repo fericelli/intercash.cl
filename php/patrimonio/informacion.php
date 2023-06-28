66e5ce75e69f099d454b7427fda1223adae40c0c
fericelli <fericelli.garcia@gmail.com>
2023-06-28 00:44:39 -0400

    a

--- php/patrimonio/informacion.php
+++ php/patrimonio/informacion.php
@@ -52,7 +52,7 @@
 						$fiats[$fiat["iso2"]][$monedaintercaionbio[0]]["cantidadinvertida"] = 0;
 						$fiats[$fiat["iso2"]][$monedaintercaionbio[0]]["disponible"] = abs(floatval($cantidadmoneda));
 						$usd = json_decode(file_get_contents("https://localbitcoins.com/api/equation/USD_in_".$monedaintercaionbio[0]))->{'data'};
-						$usdtpendientes += floatval($cantidadmoneda/$usd);
+						$usdtpendientes += floatval($cantidadmoneda/($usd*floatval("1.".$devaluacion)));
 						$fiats[$fiat["iso2"]][$monedaintercaionbio[0]]["USDT"] = floatval($cantidadmoneda/$usd);
 					}
 
