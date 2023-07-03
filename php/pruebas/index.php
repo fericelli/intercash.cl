81b1946559d7bc7e70a4b35b8ba9168670f7fb1d
fericelli <fericelli.garcia@gmail.com>
2023-07-03 00:42:19 -0400

    a

--- php/pruebas/index.php
+++ php/pruebas/index.php
@@ -5,10 +5,5 @@ if ($mysqli->connect_errno) {
 }
 echo $mysqli->host_info . "\n";
 
-$mysqli = new mysqli("127.0.0.1", "u956446715_root", "3H?eEvoa>n", "u956446715_intercash", 3306);
-if ($mysqli->connect_errno) {
-    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
 
-echo $mysqli->host_info . "\n";
 ?>
