c9e812a20dd5558f52247e185ce351549c266253
fericelli <fericelli.garcia@gmail.com>
2024-04-13 23:19:59 -0400

    a

--- php/pruebas/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("srv585.hstgr.io", "u956446715_root", "3H?eEvoa>n", "u956446715_intercash");
-if ($mysqli->connect_errno) {
-    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-echo $mysqli->host_info . "\n";
-
-
-?>
