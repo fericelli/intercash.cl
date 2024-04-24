13f03309e81b30dc32b16184f9d79d83d6594913
fericelli <fericelli.garcia@gmail.com>
2024-04-24 14:17:09 -0400

    a

--- php/cron/actualizarbtc.php
+++ php/cron/actualizarbtc.php
@@ -5,39 +5,51 @@ error_reporting(E_ALL);
 		function __construct(){
             include("/home/u956446715/public_html/public_html/php/conexion.php");
 			$this->Conexion = new Conexion();
-            //$url = 'https://pro-api.coinmarketcap.com/v2/tools/price-conversion';
-            $url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
-            /*$parameters = [
-            'amount' => '1',
-            'symbol' => 'BTC',
-            'convert' => 'USD'
-            ];*/
-            /*$parameters = [
-            'ids' => 'bitcoin',
-            'convert' => 'usd'
-            ];*/
-            /*$headers = [
-            'Accepts: application/json',
-            'X-CMC_PRO_API_KEY: cf040e5c-1049-4d6c-870d-08e1dd063018'
-            ];*/
+            $api = "CGK";
+            //$api = "CMK";
+            $url = "";
+            if($api == "CGK"){
+                $url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
+                $parameters = [
+                    'ids' => 'bitcoin',
+                    'convert' => 'usd'
+                ];
+                $headers = [
+                    'Accepts: application/json',
+                    'X-CMC_PRO_API_KEY: cf040e5c-1049-4d6c-870d-08e1dd063018'
+                ];
+            }else{
+                $url = 'https://pro-api.coinmarketcap.com/v2/tools/price-conversion';
+                $parameters = [
+                    'amount' => '1',
+                    'symbol' => 'BTC',
+                    'convert' => 'USD'
+                ];
+                $headers = [
+                    "accept: application/json",
+                    "x-cg-demo-api-key: CG-cqVau314h2vrVuPBwRWTNA2y"
+                ];
+            }
 
-            $headers = [
-                "accept: application/json",
-                "x-cg-demo-api-key: CG-cqVau314h2vrVuPBwRWTNA2y"
-            ];
+           
             
-            //$qs = http_build_query($parameters); // query string encode the parameters
-            //$request = "{$url}?{$qs}"; // create the request URL
+            $qs = http_build_query($parameters); // query string encode the parameters
+            $request = "{$url}?{$qs}"; // create the request URL
 
-            $request = "{$url}"; // create the request URL
-
+            
 
             $curl = curl_init(); // Get cURL resource
             // Set cURL options
             curl_setopt_array($curl, array(
-            CURLOPT_URL => $request,            // set the request URL
-            CURLOPT_HTTPHEADER => $headers,     // set the headers 
-            CURLOPT_RETURNTRANSFER => 1         // ask for raw response instead of bool
+                CURLOPT_URL => $request,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_HTTPHEADER => $headers, 
+                CURLOPT_RETURNTRANSFER => 1         
             ));
 
             $response = curl_exec($curl); // Send the request, save the response
