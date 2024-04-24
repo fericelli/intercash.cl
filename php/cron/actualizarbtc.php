bd6816449152b506897e518c6291f6bf2760c4cd
fericelli <fericelli.garcia@gmail.com>
2024-04-24 12:12:02 -0400

    a

--- php/cron/actualizarbtc.php
+++ php/cron/actualizarbtc.php
@@ -14,7 +14,7 @@ error_reporting(E_ALL);
             ];*/
             $parameters = [
             'ids' => 'bitcoin',
-            'convert' => 'usd'
+            'vs_currencies' => 'usd'
             ];
             /*$headers = [
             'Accepts: application/json',
