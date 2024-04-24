7d1608e2693e1a2df6baf7d3f05c9a9e3038937e
fericelli <fericelli.garcia@gmail.com>
2024-04-24 07:38:45 -0400

    a

--- php/cron/actualizarbtc.php
+++ php/cron/actualizarbtc.php
@@ -14,7 +14,7 @@ error_reporting(E_ALL);
             $preciousdbtc = $informacion["totalBid"];
 			$momento = date("Y-m-d H:i:s",$informacion["time"]);
 
-             $this->Conexion->Consultar("INSERT INTO precios (moneda,compra,venta,momento) VALUES ('BTC','".$preciousdbtc."','".$preciousdbtc."','".$momento."')");
+            echo $this->Conexion->Consultar("INSERT INTO precios (moneda,compra,venta,momento) VALUES ('BTC','".$preciousdbtc."','".$preciousdbtc."','".$momento."')");
             $this->Conexion->CerrarConexion();
             if (curl_errno($ch)) {
                 echo 'Error:' . curl_error($ch);
