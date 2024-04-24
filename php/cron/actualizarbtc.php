ffc29d99b76bf311bb0dff6cd425094394eb2c3e
fericelli <fericelli.garcia@gmail.com>
2024-04-24 05:56:57 -0400

    a

--- php/cron/actualizarbtc.php
+++ php/cron/actualizarbtc.php
@@ -5,8 +5,9 @@ error_reporting(E_ALL);
 		function __construct(){
             include("../conexion.php");
 			$this->Conexion = new Conexion();
+            echo "fesfsefe";
 			$informacion = json_decode(file_get_contents("https://criptoya.com/api/binancep2p/btc/usd/"), true);
-                echo $preciousdbtc = $informacion["totalBid"];
+            $preciousdbtc = $informacion["totalBid"];
 			$momento = date("Y-m-d H:i:s",$informacion["time"]);
 
              $this->Conexion->Consultar("INSERT INTO precios (moneda,compra,venta,momento) VALUES ('BTC','".$preciousdbtc."','".$preciousdbtc."','".$momento."')");
