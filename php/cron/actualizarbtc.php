cc2b0d7a00e377c3ca59c06ebddc856f1e54fea4
fericelli <fericelli.garcia@gmail.com>
2024-04-24 13:43:16 -0400

    a

--- php/cron/actualizarbtc.php
+++ php/cron/actualizarbtc.php
@@ -5,32 +5,15 @@ error_reporting(E_ALL);
 		function __construct(){
             include("/home/u956446715/public_html/public_html/php/conexion.php");
 			$this->Conexion = new Conexion();
-           /* $url = 'https://pro-api.coinmarketcap.com/v2/tools/price-conversion';
-            //$url = "https://api.coingecko.com/api/v3/simple/price";
+           
+            $url = 'https://pro-api.coinmarketcap.com/v2/tools/price-conversion';
             $parameters = [
-            'amount' => '1',
-            'symbol' => 'BTC',
-            'convert' => 'USD'
+                'amount' => '1',
+                'symbol' => 'BTC',
+                'convert' => 'USD'
             ];
-            /*$parameters = [
-            'ids' => 'bitcoin',
-            'vs_currencies' => 'usd'
-            ];*/
-            /*$headers = [
-            'Accepts: application/json',
-            'X-CMC_PRO_API_KEY: cf040e5c-1049-4d6c-870d-08e1dd063018'
-            ];
-
-            /*$headers = [
-                "accept: application/json",
-                "x-cg-demo-api-key: CG-cqVau314h2vrVuPBwRWTNA2y"
-            ];*/
-            
-            /*$qs = http_build_query($parameters); // query string encode the parameters
+            $qs = http_build_query($parameters); // query string encode the parameters
             $request = "{$url}?{$qs}"; // create the request URL
-
-            //$request = "{$url}"; // create the request URL
-
 
             $curl = curl_init(); // Get cURL resource
             // Set cURL options
@@ -47,28 +30,9 @@ error_reporting(E_ALL);
             ));
 
             $response = curl_exec($curl); // Send the request, save the response
-            print_r(json_decode($response));
-            //print_r(json_decode($response)->{"data"}[0]->{"quote"}->{"USD"}->{"price"}); // print json decoded response
+            //print_r(json_decode($response));
             curl_close($curl); // Close request
-           /* $ch = curl_init();
-
-            curl_setopt($ch, CURLOPT_URL, 'https://criptoya.com/api/binancep2p/btc/usd');
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-            $informacion = json_decode(curl_exec($ch), true);
-            $preciousdbtc = $informacion["totalBid"];
-			$momento = date("Y-m-d H:i:s",$informacion["time"]);
-            $this->Conexion->Consultar("INSERT INTO precios (moneda,compra,venta,momento) VALUES ('BTC','".$preciousdbtc."','".$preciousdbtc."','".$momento."')");
-            $this->Conexion->CerrarConexion();
-            if (curl_errno($ch)) {
-                echo 'Error:' . curl_error($ch);
-            }
-            //print_r($result);
-            curl_close($ch);*/
-			/* = json_decode(file_get_contents("https://criptoya.com/api/binancep2p/btc/usd/"), true);*/
-            $informacion = $this->ConexionCoinMarkCap();
-            
-            print_r($informacion);
+            echo json_decode($response)->{"data"}[0]->{"quote"}->{"USD"}->{"price"}; // print json decoded response
 
             /*$preciousdbtc = $informacion["totalBid"];
 			$momento = date("Y-m-d H:i:s",$informacion["time"]);
