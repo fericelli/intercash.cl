832fe51b9991f3540a5b277d24450dc6d40a9b4f
fericelli <fericelli.garcia@gmail.com>
2024-04-24 08:22:27 -0400

    a

--- php/cron/actualizarbtc.php
+++ php/cron/actualizarbtc.php
@@ -5,7 +5,29 @@ error_reporting(E_ALL);
 		function __construct(){
             include("/home/u956446715/public_html/public_html/php/conexion.php");
 			$this->Conexion = new Conexion();
-            $ch = curl_init();
+            $url = 'https://pro-api.coinmarketcap.com';
+            
+
+            $headers = [
+            'Accepts: application/json',
+            'X-CMC_PRO_API_KEY: cf040e5c-1049-4d6c-870d-08e1dd063018'
+            ];
+           // $qs = http_build_query($parameters); // query string encode the parameters
+            $request = "{$url}";//?{$qs}"; // create the request URL
+
+
+            $curl = curl_init(); // Get cURL resource
+            // Set cURL options
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => $request,            // set the request URL
+            CURLOPT_HTTPHEADER => $headers,     // set the headers 
+            CURLOPT_RETURNTRANSFER => 1         // ask for raw response instead of bool
+            ));
+
+            $response = curl_exec($curl); // Send the request, save the response
+            print_r(json_decode($response)); // print json decoded response
+            curl_close($curl); // Close request
+           /* $ch = curl_init();
 
             curl_setopt($ch, CURLOPT_URL, 'https://criptoya.com/api/binancep2p/btc/usd');
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -13,14 +35,13 @@ error_reporting(E_ALL);
             $informacion = json_decode(curl_exec($ch), true);
             $preciousdbtc = $informacion["totalBid"];
 			$momento = date("Y-m-d H:i:s",$informacion["time"]);
-            echo "INSERT INTO precios (moneda,compra,venta,momento) VALUES ('BTC','".$preciousdbtc."','".$preciousdbtc."','".$momento."')";
-            echo $this->Conexion->Consultar("INSERT INTO precios (moneda,compra,venta,momento) VALUES ('BTC','".$preciousdbtc."','".$preciousdbtc."','".$momento."')");
+            $this->Conexion->Consultar("INSERT INTO precios (moneda,compra,venta,momento) VALUES ('BTC','".$preciousdbtc."','".$preciousdbtc."','".$momento."')");
             $this->Conexion->CerrarConexion();
             if (curl_errno($ch)) {
                 echo 'Error:' . curl_error($ch);
             }
             //print_r($result);
-            curl_close($ch);
+            curl_close($ch);*/
 			/* = json_decode(file_get_contents("https://criptoya.com/api/binancep2p/btc/usd/"), true);
             $preciousdbtc = $informacion["totalBid"];
 			$momento = date("Y-m-d H:i:s",$informacion["time"]);
