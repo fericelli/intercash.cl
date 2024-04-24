2be0d4193137533cc72d42a610416f60149ab6df
fericelli <fericelli.garcia@gmail.com>
2024-04-23 20:16:14 -0400

    cron

---
+++ php/cron/actualizarbtc.php
@@ -0,0 +1,17 @@
+<?php
+	Class ActualizarBtc{
+		private $Conexion;
+		function __construct(){
+			include("../conexion.php");
+			$this->Conexion = new Conexion();
+			$informacion = json_decode(file_get_contents("https://criptoya.com/api/binancep2p/btc/usd/"), true);
+            $preciousdbtc = $informacion["totalBid"];
+			$momento = date("Y-m-d H:i:s",$informacion["time"]);
+
+            $this->Conexion->Consultar("INSERT INTO precios (moneda,compra,venta,momento) VALUES ('BTC','".$preciousdbtc."','".$preciousdbtc."','".$momento."')");
+            $this->Conexion->CerrarConexion();
+        }
+		
+	}
+	new ActualizarBtc();
+?>
---
+++ php/cron/actualizarusdt.php
@@ -0,0 +1,37 @@
+<?php
+	Class ActualizarUsdt{
+		private $Conexion;
+		function __construct(){
+			include("../conexion.php");
+			$this->Conexion = new Conexion();
+			$values = "";
+			$update = "";
+			$consultar = $this->Conexion->Consultar("SELECT monedaventa FROM tasas GROUP BY monedaventa");
+			while($monedas = $this->Conexion->Recorrido($consultar)){
+				$moneda = $monedas[0];
+				if($monedas[0]=="VED"){
+					$moneda = "VES";
+				}
+				try{
+					$informacion = json_decode(file_get_contents("https://criptoya.com/api/binancep2p/USDT/".$moneda."/"), true);
+					$preciocompra = $informacion["totalAsk"];
+					$precioventa = $informacion["totalBid"];
+					$momento = date("Y-m-d H:i:s",$informacion["time"]);
+					
+					if(!is_null($precioventa) AND $monedas[0]<>"USD"){
+						$values .=  "('".$monedas[0]."','".$preciocompra."','".$precioventa."','".$momento."'),";
+						$this->Conexion->Consultar("UPDATE tasas SET anunciocompra='".$preciocompra."' WHERE monedacompra='".$monedas[0]."'");
+						$this->Conexion->Consultar("UPDATE tasas SET anuncioventa='".$precioventa."' WHERE monedaventa='".$monedas[0]."'");
+					}
+				}catch(Exception $e){
+					
+				}
+			} 
+			$values = substr($values,0,strlen($values)-1);
+			$this->Conexion->Consultar("INSERT INTO precios (moneda,compra,venta,momento) VALUES ".$values."");
+			$this->Conexion->CerrarConexion();
+		}
+		
+	}
+	new ActualizarUsdt();
+?>
