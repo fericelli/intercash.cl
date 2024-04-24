927d4ca32bc79dbf13ca6cf4fd54dc1053f4da24
fericelli <fericelli.garcia@gmail.com>
2024-04-24 06:15:14 -0400

    a

--- php/cron/actualizarbtc.php
+++ php/cron/actualizarbtc.php
@@ -3,15 +3,25 @@ error_reporting(E_ALL);
 	Class ActualizarBtc{
 		private $Conexion;
 		function __construct(){
-            include("../conexion.php");
-			$this->Conexion = new Conexion();
-            echo "fesfsefe";
-			$informacion = json_decode(file_get_contents("https://criptoya.com/api/binancep2p/btc/usd/"), true);
+            /*include("../../conexion.php");
+			$this->Conexion = new Conexion();*/
+            $ch = curl_init();
+
+            curl_setopt($ch, CURLOPT_URL, 'https://criptoya.com/api/binancep2p/btc/usd');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+            $result = curl_exec($ch);
+            if (curl_errno($ch)) {
+                echo 'Error:' . curl_error($ch);
+            }
+            print_r($result);
+            curl_close($ch);
+			/*$informacion = json_decode(file_get_contents("https://criptoya.com/api/binancep2p/btc/usd/"), true);
             $preciousdbtc = $informacion["totalBid"];
 			$momento = date("Y-m-d H:i:s",$informacion["time"]);
 
              $this->Conexion->Consultar("INSERT INTO precios (moneda,compra,venta,momento) VALUES ('BTC','".$preciousdbtc."','".$preciousdbtc."','".$momento."')");
-            $this->Conexion->CerrarConexion();
+            $this->Conexion->CerrarConexion();*/
         }
 		
 	}
